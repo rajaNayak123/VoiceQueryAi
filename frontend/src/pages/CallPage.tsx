a5c95610f6030e3d4f8f1b9f52f6653538e95ba1
rajaNayak123 <nayakraja151@gmail.com>
2026-09-09 11:19:54 +0530

    pass document id and filename props to call room

--- frontend/src/pages/CallPage.tsx
+++ frontend/src/pages/CallPage.tsx
@@ -3,14 +3,27 @@ import { CallRoom } from "../components/call/CallRoom";
 interface CallPageProps {
   token: string;
   livekitUrl: string;
+  documentId?: string;
+  filename?: string;
   onCallEnded: () => void;
 }
 
-export function CallPage({ token, livekitUrl, onCallEnded }: CallPageProps) {
+export function CallPage({
+  token,
+  livekitUrl,
+  documentId,
+  filename,
+  onCallEnded,
+}: CallPageProps) {
   return (
-    <div style={{ maxWidth: 640, margin: "40px auto", fontFamily: "sans-serif" }}>
-      <h1 style={{ marginBottom: 24 }}>Live conversation</h1>
-      <CallRoom token={token} livekitUrl={livekitUrl} onCallEnded={onCallEnded} />
+    <div className="call-page-wrapper">
+      <CallRoom
+        token={token}
+        livekitUrl={livekitUrl}
+        documentId={documentId}
+        filename={filename}
+        onCallEnded={onCallEnded}
+      />
     </div>
   );
 }
